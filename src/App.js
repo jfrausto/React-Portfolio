22fe73872baa0a05f0d85118573c0dbf70f91cb4
jessef <1319957+jfrausto@users.noreply.github.com>
2020-11-16 23:28:02 -0800

    create Router/Navigation skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import NavigationTabs from "./components/NavigationTabs";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/*  <NavigationTabs /> */}
+        {/* 
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        
+        */}
+      </div>
+    </Router>
+    
   );
 }
 
