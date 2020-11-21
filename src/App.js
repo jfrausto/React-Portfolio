e051a5e6c1d05a64610ee3eecf6385da3b0b288d
jessef <1319957+jfrausto@users.noreply.github.com>
2020-11-21 01:42:50 -0800

    refactor to react-bootstrap components; write navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Header from "./components/Header";
 import NavigationTabs from "./components/NavigationTabs";
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     // <Router>
-      <div className="App container-fluid">
+      <div className="App roboto">
         {/* Header component */}
         {/* <Header /> */}
         {/*  Navigation Component */}
@@ -20,6 +20,9 @@ function App() {
         <Route path="/contact" component={Contact} />
         
         */}
+        {/* <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script> */}
       </div>
     // </Router>
     
